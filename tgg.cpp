4a39c4fd0a5e1ea2b973fd44f6204c87db7a6c88
Ivan Prihodchenko <vanek3478989@gmail.com>
2019-06-29 10:50:12 -0400

    Grid for 8 teams (Working version)

--- tgg.cpp
+++ tgg.cpp
@@ -16,7 +16,7 @@ const char windowname[] = "Tournament grid generator";//Íàçâàíèå îêí�
 typedef char text[ttl+1];//Ïåðåìåííàÿ äëÿ ìàññèâà íàçâàíèé êîìàíä
 
 //===============[Ãëîáàëüíûå ïåðåìåííûå]===============
-text buffer[32] = {"team# 1","team# 2","team# 3","team# 4","team# 5","team# 6","team# 7","team# 8","team# 9","team# 10","team# 11","team# 12","team# 13","team# 14","team# 15","team# 16","team# 17","team# 18","team# 19","team# 20","team# 21","team# 22","team# 23","team# 24","team# 25","team# 26","team# 27","team# 28","team# 29","team# 30","team# 31","team# 32"};//Ìàêñèìàëüíîå êîëè÷åñâî êîìàíä (ìàññèâ íàçâàíèé)
+text buffer[32];//Ìàêñèìàëüíîå êîëè÷åñâî êîìàíä (ìàññèâ íàçâàíèé)
 int placegrid[32];//Ìàêñèìàëüíîå êîëè÷åñâî êîìàíä (ìàññèâ çàíÿòûõ ìåñò)
 
 int grid4(int);
@@ -30,9 +30,9 @@ main()
 	system("color 80");
 	srand(time(0));
 	char inputtn[100];
-	int numbert = 8;
+	int numbert;
 	string check;
-	/*cout<<"Ââåäèòå êîëè÷åñòâî êîìàíä ðàâíîå 4/8/16/32."<<endl;
+	cout<<"Ââåäèòå êîëè÷åñòâî êîìàíä ðàâíîå 4/8/16/32."<<endl;
 	cin>>numbert;
 	while(numbert!=4 && numbert!=8 && numbert!=16 && numbert!=32)
 	{
@@ -52,7 +52,7 @@ main()
 			cin>>inputtn;
 		}
 		strcpy(buffer[i], inputtn);
-	}*/
+	}
 	switch(numbert)
 	{
 		case 4: 
