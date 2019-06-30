ac56915def0d34ba14143d914532606500071023
Vladislav Filimonov <new1746@mail.ru>
2019-06-30 22:41:15 +0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("color 80");
+	srand(time(0));
+	char inputtn[100];
+	int numbert = 8;
+	string check;
+	cout<<"Введите количество команд равное 4/8/16/32."<<endl;
+	cin>>numbert;
+	while(numbert!=4 && numbert!=8 && numbert!=16 && numbert!=32)
+	{
+		numbert = 0;
+		cout<<"Код ошибки: 1. Количество команд должно быть равно 4/8/16/32."<<endl;
+		cin>>numbert;
+	}
+	for(int i=0; i<numbert; i++)
+	{
+		cout<<"Введите название команды "<<i+1<<" на английском языке:"<<endl;
+		cin>>inputtn;
+		while(strlen(inputtn)>ttl)
+		{
+			strcpy(inputtn,"");
+			cout<<inputtn<<endl;
+			cout<<"Код ошибки: 2. Количество символов не должно превышать "<<ttl<<"."<<endl;
+			cin>>inputtn;
+		}
+		strcpy(buffer[i], inputtn);
+	}
+	switch(numbert)
+	{
+		case 4: 
+		{
+			grid4(numbert);
+			break;
+		}
+		case 8:
+		{
+			grid8(numbert);
+			break;
+		}
+		case 16:
+		{
+			break;
+		}
+		case 32:
+		{
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
