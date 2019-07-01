f28f0f4931d01472d7f172beb6ff5535b13390a8
Vladislav Filimonov <new1746@mail.ru>
2019-07-01 08:32:05 +0700

    Create new func.h

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.com/new1746/cw-tg-generator.svg?branch=master)](https://travis-ci.com/new1746/cw-tg-generator)
--- func.h
+++ func.h
@@ -3,7 +3,7 @@ const char windowname[] = "Tournament grid generator";
 
 typedef char text[ttl+1];
 
-text buffer[32] = {"team# 1","team# 2","team# 3","team# 4","team# 5","team# 6","team# 7","team# 8","team# 9","team# 10","team# 11","team# 12","team# 13","team# 14","team# 15","team# 16","team# 17","team# 18","team# 19","team# 20","team# 21","team# 22","team# 23","team# 24","team# 25","team# 26","team# 27","team# 28","team# 29","team# 30","team# 31","team# 32"};//Максимальное количесво команд (массив названий)
+text buffer[32] = {"team# 1","team# 2","team# 3","team# 4","team# 5","team# 6","team# 7","team# 8","team# 9","team# 10","team# 11","team# 12","team# 13","team# 14","team# 15","team# 16","team# 17","team# 18","team# 19","team# 20","team# 21","team# 22","team# 23","team# 24","team# 25","team# 26","team# 27","team# 28","team# 29","team# 30","team# 31","team# 32"};//ДљЕ•Д™Е„ДЌД›Е•Г«ГјГ­Г®Дє Д™Г®Г«ДЌГ·ДєЕ„ГўГ® Д™Г®Д›Е•Г­Г¤ (Д›Е•Е„Е„ДЌГў Г­Е•Г§ГўЕ•Г­ДЌГ©)
 
 int placegrid[32];
 int grid4(int);
