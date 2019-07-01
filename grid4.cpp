0d6506fef918ec219327a68e12a3ef06cb33e34c
Ivan Prihodchenko <vanek3478989@gmail.com>
2019-07-01 08:39:21 +0700

    Create grid4.cpp

---
+++ grid4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "func.h"
+
+using namespace std;
+
+int grid4(int numt)
+{
+	int teamsN[6] = {0,1,-1,-1,2,3}, winner = -1;
+	int key; 
+	for(int i =0; i<6; i++)
+	{
+		if(teamsN[i]!=-1) cout<<i<<") "<<buffer[teamsN[i]]<<endl;
+		else cout<<"  \t\t\t??"<<endl;
+	}
+    while(winner==-1)
+	{
+		cin>>key;
+		system("clear");
+		if((teamsN[2]==key || teamsN[3]==key) && teamsN[2]!=-1 && teamsN[3]!=-1) winner = teamsN[key];
+		if((key==0 || key==1) && teamsN[2] == -1) teamsN[2] = teamsN[key];
+		if((key==4 || key==5) && teamsN[3] == -1) teamsN[3] = teamsN[key];
+		for(int i =0; i<6; i++)
+		{
+			if(teamsN[i]!=-1) 
+				if(i == 2 || i == 3) cout<<"  \t\t\t"<<i<<")"<<buffer[teamsN[i]]<<endl; 
+				else cout<<i<<") "<<buffer[teamsN[i]]<<endl;
+			else cout<<"  \t\t\t??"<<endl;
+		}
+	}
+	cout<<endl<<"winner: "<<buffer[winner]<<endl;
+	return 0;
+}
